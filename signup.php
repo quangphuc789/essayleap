9b2707f02494709b54baff47cb897fe292ccefcb
QP <qp@powerme.com.sg>
2016-09-02 12:58:44 +0800

    added signup ui page

---
+++ signup.php
@@ -0,0 +1,40 @@
+<html>
+    <head>
+    </head>
+    <body>
+        <table>
+            <tr>
+                <td>Last Name</td>
+                <td><input type='text'/></td>
+            </tr>
+            <tr>
+                <td>First Name</td>
+                <td><input type='text'/></td>
+            </tr>
+            <tr>
+                <td>Email</td>
+                <td><input type='text'/></td>
+            </tr>
+            <tr>
+                <td>Password</td>
+                <td><input type='password'/></td>
+            </tr>
+            <tr>
+                <td>Confirm Password</td>
+                <td><input type='password'/></td>
+            </tr>
+            <tr>
+                <td>Primary Language</td>
+                <td><input type='text'/></td>
+            </tr>
+            <tr>
+                <td>Country</td>
+                <td><input type='text'/></td>
+            </tr>
+            <tr>
+                <td>Date of Birtd</td>
+                <td><input type='date'/></td>
+            </tr>
+        </table>
+    </body>
+</html>
