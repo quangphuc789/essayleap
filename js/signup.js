b54aaadbd938dc9d7d68df21c9f5209bde955b1c
QP <qp@powerme.com.sg>
2016-09-03 02:46:37 +0800

    major modification on the signup js logic

--- js/signup.js
+++ js/signup.js
@@ -31,11 +31,27 @@ function submitSignup() {
     } else {
         // Check for the password match
         if (info.password[0] == info.confirmedpassword[0]) {
-            console.log()
-            console.log('fine. to send ajax');
+            if (validateEmail(info.email[0])) {
+                if (info.password[0].length < 6) {
+                    alert('Password must be more than 6 letters and/or digits');
+                    document.getElementById('password').parentNode.style.border
+                        = '1px solid red';
+                    document.getElementById('confirmedpassword').parentNode.style.border
+                        = '1px solid red';
+                } else {
+                    console.log('OK I am fine');
+                }
+            } else {
+                document.getElementById('email').parentNode.style.border = '1px solid red';
+                alert('Email is not in right format');
+            }
         } else {
             alert('Password mismatched. Please type again.');
         }
     }
 
+    function validateEmail(email) {
+        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
+    }
 }
